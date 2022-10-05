c3f97739e9680ffc1f77f4f3783b07c507abcb36
SISULU255 <97722123+SISULU255@users.noreply.github.com>
2022-10-05 12:56:24 +0300

    Update Qn_An.py

--- Qn_An.py
+++ Qn_An.py
@@ -21,17 +21,17 @@ predictions = predictor.predict_json({
       "film written and directed by The Wachowskis, "
       "starring Keanu Reeves, Laurence Fishburne, "
       "Carrie-Anne Moss, Hugo Weaving, and Joe P"
-      "antoliano."
+      "antoliano.\n"
       "KIITEC is a technical institution registered by"
       "NACTE (REG/EOS/027) based in Moshono,"
-      "Arusha next to Masai Camp"
+      "Arusha next to Masai Camp\n"
       "Fee structure and Mode of Payment for Diploma Programmes,"
       "for first semister is 695,000Tsh can be paid in two installments"
       "before the end of the semester, and for second semister is 625,000Tsh,"
-      "Fee in the second semester can be paid in two installments before the end of the semester."
+      "Fee in the second semester can be paid in two installments before the end of the semester.\n"
       "The fees should be paid through the BANK of  ABSA"
       "and the Account number is  002-4001687 "
-      "the Account Name is  KIITEC Ltd.",
+      "the Account Name is  KIITEC Ltd.\n",
   "question":st.text_input('Question', 'what is Kiitec')
 })
 st.write(predictions["best_span_str"])
