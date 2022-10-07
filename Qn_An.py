06f8b0f9249cf725d9bfc560df02128d42ca2514
SISULU255 <97722123+SISULU255@users.noreply.github.com>
2022-10-07 15:14:13 +0300

    Update Qn_An.py

--- Qn_An.py
+++ Qn_An.py
@@ -11,20 +11,20 @@ image = Image.open('kiitec logo.png')
 #html = "<img src= "kiitec logo.png" >"
 #css = "img{diplay:relative,left:350}"
 st.image(image,  width=None)
+st.title('''KIITEC VIRTUAL ASSISTANT''')
 checkpoint = "https://storage.googleapis.com/allennlp-public-models/bidaf-elmo.2021-02-11.tar.gz"
 #checkpoint = "hf://lysandre/bidaf-elmo-model-2020.03.19"
-st.title('''KIITEC VIRTUAL ASSISTANT''')
 predictor = Predictor.from_path(checkpoint)
 predictions = predictor.predict_json({
   "passage":
-      "The Matrix is a 1999 science fiction action "
-      "film written and directed by The Wachowskis, "
-      "starring Keanu Reeves, Laurence Fishburne, "
-      "Carrie-Anne Moss, Hugo Weaving, and Joe P"
-      "antoliano.\n"
+      #"The Matrix is a 1999 science fiction action "
+      #"film written and directed by The Wachowskis, "
+      #"starring Keanu Reeves, Laurence Fishburne, "
+      #"Carrie-Anne Moss, Hugo Weaving, and Joe P"
+      #"antoliano.\n"
       "KIITEC is a technical institution registered by"
       "NACTE (REG/EOS/027) based in Moshono,"
-      "Arusha next to Masai Camp\n"
+      "Arusha next to Masai Camp."\n
       "Fee structure and Mode of Payment for Diploma Programmes,"
       "for first semister is 695,000Tsh can be paid in two installments"
       "before the end of the semester, and for second semister is 625,000Tsh,"
