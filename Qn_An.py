3100fce431ce261ad02dd6a20fe5d9c474374556
SISULU255 <97722123+SISULU255@users.noreply.github.com>
2022-10-10 14:16:24 +0300

    Update Qn_An.py

--- Qn_An.py
+++ Qn_An.py
@@ -24,15 +24,15 @@ predictions = predictor.predict_json({
       #"Carrie-Anne Moss, Hugo Weaving, and Joe P"
       #"antoliano.\n"
       "KIITEC is a technical institution registered by "
-      "NACTE (REG/EOS/027) based in Moshono,"
+      "NACTE (REG/EOS/027) based in Moshono, "
       "Arusha next to Masai Camp.\n"
-      "Fee structure and Mode of Payment for Diploma Programmes,"
-      "for first semister is 695,000Tsh can be paid in two installments"
-      "before the end of the semester, and for second semister is 625,000Tsh,"
-      "Fee in the second semester can be paid in two installments before the end of the semester.\n"
-      "The fees should be paid through the BANK of  ABSA"
+      "Fee structure and Mode of Payment for Diploma Programmes, "
+      "for first semister is 695,000Tsh can be paid in two installments "
+      "before the end of the semester, and for second semister is 625,000Tsh, "
+      "Fee in the second semester can be paid in two installments before the end of the semester.\n "
+      "The fees should be paid through the BANK of  ABSA "
       "and the Account number is  002-4001687 "
-      "the Account Name is  KIITEC Ltd.\n",
+      "the Account Name is  KIITEC Ltd.\n" ,
   "question":st.text_input('Question', 'what is Kiitec')
 })
 st.write(predictions["best_span_str"])
